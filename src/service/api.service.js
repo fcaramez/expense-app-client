a09dfd92ad4f66589acaca95c87f6198458ab2c6
fcaramez <francisco.caramez.duarte@gmail.com>
2022-08-07 01:14:30 +0100

    adds first files, creates API service and adds first packages

---
+++ src/service/api.service.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+class apiService {
+  constructor() {
+    this.baseUrl = process.env.REACT_APP_API_URL;
+    this.api = axios.create({
+      baseURL: this.baseUrl,
+    });
+  }
+
+  getExpenses(apiKey) {
+    return this.api.get(`/api/expenses/${apiKey}`);
+  }
+
+  createExpense(apiKey, body) {
+    return this.api.post(`/api/expenses/${apiKey}`, body);
+  }
+
+  updateExpense(apiKey, expenseId, body) {
+    return this.api.put(`/api/expense/${expenseId}/${apiKey}`, body);
+  }
+
+  deleteExpense(apiKey, expenseId) {
+    return this.api.delete(`/api/expense/${expenseId}/${apiKey}`);
+  }
+}
+
+export default apiService;
