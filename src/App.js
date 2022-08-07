1eb3da0d3ad32be7889e3a753c030af86447df55
fcaramez <francisco.caramez.duarte@gmail.com>
2022-08-07 01:51:03 +0100

    adds basic routes, finishes authentication on the frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
-
+import { Routes, Route } from "react-router-dom";
+import IsAnon from "./components/IsAnon";
+import IsPrivate from "./components/IsPrivate";
 
 function App() {
-  return <div className="App">
-    <SignupPage />
-  </div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route
+          path={"/login"}
+          element={
+            <IsAnon>
+              <LoginPage />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <IsAnon>
+              <SignupPage />
+            </IsAnon>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 import axios from "axios";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const { storeToken, authenticateUser } = useContext(AuthContext);
+
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleEmail = (e) => setEmail(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const body = { email, password };
+
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/auth/login`, body)
+      .then((response) => {
+        storeToken(response.data.authToken);
+        authenticateUser();
+        navigate("/");
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
+  return (
+    <div className="LoginPage">
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Name</label>
+        <input type="text" name="email" value={email} onChange={handleEmail} />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePassword}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+
+      <p>Don't have an account?</p>
+      <Link to="/signup"> Sign up</Link>
+    </div>
+  );
 }
 
 export default LoginPage;
