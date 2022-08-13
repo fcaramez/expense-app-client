64714199441957a2f647cf87d56f7d76a2c2012f
fcaramez <francisco.caramez.duarte@gmail.com>
2022-08-13 17:26:33 +0100

    refactors expense create

--- src/pages/ExpenseCreate.jsx
+++ src/pages/ExpenseCreate.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-import expenseService from "../service/expense.service";
-import { useState, useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/auth.context";
 import {
-  InputGroup,
-  Button,
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
   Input,
+  InputGroup,
+  HStack,
   Stack,
+  Button,
   Heading,
-  Text,
   NumberInputField,
   NumberInput,
   Select,
 } from "@chakra-ui/react";
+import expenseService from "../service/expense.service";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 
 function ExpenseCreate() {
   const [source, setSource] = useState("");
@@ -46,79 +50,106 @@ function ExpenseCreate() {
   };
 
   return (
-    <>
-      <Heading textAlign={"center"}>Create an Expense</Heading>
-      <br />
-      <form onSubmit={handleSubmit}>
-        <Stack spacing={3}>
-          <Text>Source: </Text>
-          <InputGroup>
-            <Select
-              placeholder="Select source"
-              onChange={(e) => setSource(e.target.value)}
-              value={source}>
-              <option value="income">Income</option>
-              <option value="expense">Expense</option>
-            </Select>
-          </InputGroup>
-          <Text>Name: </Text>
-          <InputGroup>
-            <Input
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Ex: 08/2022 Salary"
-              size="sm"
-            />
-          </InputGroup>
-          <Text>Price: </Text>
-          <InputGroup>
-            <NumberInput width={"100%"}>
-              <NumberInputField
-                placeholder="Price"
-                width={"100%"}
-                onChange={(e) => setPrice(e.target.value)}
-                value={price}
-              />
-            </NumberInput>
-          </InputGroup>
-          <Text>Type: </Text>
-          <InputGroup>
-            <Select
-              placeholder="Select type"
-              onChange={(e) => setType(e.target.value)}
-              value={type}>
-              <option value="Household">Household</option>
-              <option value="Technology">Technology</option>
-              <option value="Work">Work</option>
-              <option value="Salary">Salary</option>
-              <option value="Allowance">Allowance</option>
-              <option value="Transportation">Transportation</option>
-              <option value="Entertainment">Entertainment</option>
-              <option value="Going Out">Going Out</option>
-              <option value="Other">Other</option>
-            </Select>
-          </InputGroup>
-          <Text>Date:</Text>
-          <InputGroup>
-            <Input
-              placeholder="Select Date and Time"
-              size="md"
-              type="datetime-local"
-              onChange={(e) => setDate(e.target.value)}
-              value={date}
-              max={"today"}
-            />
-          </InputGroup>
-          <br />
-          <InputGroup alignItems={"center"}>
-            <Button width={"100%"} colorScheme="teal" type="submit">
-              Create Expense
-            </Button>
-          </InputGroup>
-          {errorMessage && <Text>{errorMessage}</Text>}
+    <form onSubmit={handleSubmit}>
+      <Flex minH={"100vh"} align={"center"} justify={"center"}>
+        <Stack
+          spacing={8}
+          mx={"auto"}
+          maxW={"lg"}
+          width={"100%"}
+          py={12}
+          px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"} textAlign={"center"}>
+              Create an Entry
+            </Heading>
+          </Stack>
+          <Box rounded={"lg"} boxShadow={"lg"} p={8}>
+            <Stack spacing={4}>
+              <HStack>
+                <FormControl id="username" isRequired>
+                  <FormLabel>Source</FormLabel>
+                  <Select
+                    placeholder="Select source"
+                    onChange={(e) => setSource(e.target.value)}
+                    value={source}>
+                    <option value="income">Income</option>
+                    <option value="expense">Expense</option>
+                  </Select>
+                </FormControl>
+              </HStack>
+              <FormControl isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input
+                  type="text"
+                  placeholder="Ex: 08/2022 Salary"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>€ Price</FormLabel>
+                <InputGroup>
+                  <NumberInput width={"100%"}>
+                    <NumberInputField
+                      placeholder="Price"
+                      width={"100%"}
+                      onChange={(e) => setPrice(e.target.value)}
+                      value={price}
+                    />
+                  </NumberInput>
+                </InputGroup>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Category</FormLabel>
+                <InputGroup>
+                  <Select
+                    placeholder="Select type"
+                    onChange={(e) => setType(e.target.value)}
+                    value={type}>
+                    <option value="Household">Household</option>
+                    <option value="Technology">Technology</option>
+                    <option value="Work">Work</option>
+                    <option value="Salary">Salary</option>
+                    <option value="Allowance">Allowance</option>
+                    <option value="Transportation">Transportation</option>
+                    <option value="Entertainment">Entertainment</option>
+                    <option value="Going Out">Going Out</option>
+                    <option value="Other">Other</option>
+                  </Select>
+                </InputGroup>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Date:</FormLabel>
+                <InputGroup>
+                  <Input
+                    placeholder="Select Date and Time"
+                    size="md"
+                    type="datetime-local"
+                    onChange={(e) => setDate(e.target.value)}
+                    value={date}
+                    max={"today"}
+                  />
+                </InputGroup>
+              </FormControl>
+              <Stack spacing={10} pt={2}>
+                <Button
+                  loadingText="Submitting"
+                  size="lg"
+                  colorScheme={"green"}
+                  bg={"green.400"}
+                  rounded={"full"}
+                  _hover={{
+                    bg: "green.500",
+                  }}>
+                  Create Entry
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
         </Stack>
-      </form>
-    </>
+      </Flex>
+    </form>
   );
 }
 
